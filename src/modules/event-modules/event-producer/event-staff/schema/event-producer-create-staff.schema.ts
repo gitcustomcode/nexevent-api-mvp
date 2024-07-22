f58b072d5cac66b08f0e1924824d19e00807e53b
gitcustomcode <desenvolvimento@customcode.com.br>
2024-07-22 14:32:27 -0300

    refactor: altered create staff schema

--- src/modules/event-modules/event-producer/event-staff/schema/event-producer-create-staff.schema.ts
+++ src/modules/event-modules/event-producer/event-staff/schema/event-producer-create-staff.schema.ts
@@ -1,8 +1,5 @@
 import { z } from 'nestjs-zod/z';
 
-export const EventProducerCreateStaffSchema = z.array(
-  z.object({
-    email: z.string(),
-    password: z.string(),
-  }),
-);
+export const EventProducerCreateStaffSchema = z.object({
+  email: z.string().email(),
+});
