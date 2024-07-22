a3738c27641200da3087f47bc1431b37dc08d572
gitcustomcode <desenvolvimento@customcode.com.br>
2024-07-22 15:41:53 -0300

    feat: created schema for service recommend staffs

--- src/modules/event-modules/event-producer/event-staff/schema/event-producer-response-staff.schema.ts
+++ src/modules/event-modules/event-producer/event-staff/schema/event-producer-response-staff.schema.ts
@@ -1,8 +1,23 @@
 import { z } from 'nestjs-zod/z';
-
 export const EventProducerResponseCreateStaffSchema = z.array(
   z.object({
     id: z.string(),
     email: z.string().email(),
   }),
 );
+
+export const EventProducerRecommendedStaffSchema = z.object({
+  staffId: z.string(),
+  staffName: z.string(),
+  staffEmail: z.string().email(),
+
+  eventCount: z.number(),
+
+  events: z.array(
+    z.object({
+      eventId: z.string(),
+      eventTitle: z.string(),
+      eventDate: z.date(),
+    }),
+  ),
+});
